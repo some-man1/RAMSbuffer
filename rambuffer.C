888859b82523f79e7a4ae41944577436130d1d16
some-man1 <142589483+some-man1@users.noreply.github.com>
2023-12-29 15:25:22 -0500

    src
    src

---
+++ rambuffer.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    while (1){
+    for(unsigned long long int x = 0; x <= 10000000000;x++){
+       int *array = (int *)malloc(100000000 * sizeof(int));
+       int *arraytwo = (int *)malloc(100000000 * sizeof(int));
+        int *arraythree = (int *)malloc(100000000 * sizeof(int));
+        int *arrayfour = (int *)malloc(100000000 * sizeof(int));
+        int *arrayfive = (int *)malloc(100000000 * sizeof(int));
+        int *arraysix = (int *)malloc(100000000 * sizeof(int));
+    }     
+    }
+}
+
